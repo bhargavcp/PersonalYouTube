d1d06836af089cd2d53f97c8838742552a11b090
bhargavcp <pandyab@oregonstate.edu>
2018-02-18 22:15:14 -0800

    Added config file.

---
+++ config/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    configuration: {
+        account: {
+            base_url: 'https://www.googleapis.com/youtube/v3',
+            key: 'REPLACE_THIS_TEXT_WITH_YOUR_KEY',
+        },
+    },
+    videoViewed: { data: { id: 'ktafrbsKeZw' } },
+};
